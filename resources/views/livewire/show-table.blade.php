93078df211d8e652467fbaa420cb750c1c3aa863
tonynnabs <tonynnabs01@gmail.com>
2021-07-15 11:56:38 -0400

    header

--- resources/views/livewire/show-table.blade.php
+++ resources/views/livewire/show-table.blade.php
@@ -1,6 +1,12 @@
 <div>
     <x-loading />
-    <div class="justify-end items-center mt-5">
+    <div class="flex justify-between items-center mt-5">
+        <a href="https://github.com/tonynnabs/data-max-test" class="flex items-center">
+            <h1 class="text-3xl font-bold">Github Repo</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-6 w-6 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+            </svg>
+        </a>
         <div class="flex justify-end items-center">
             <h3 class="mr-3">Search</h3>
             <div>
